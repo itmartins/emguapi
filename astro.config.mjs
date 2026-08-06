acb07582953ad7a4a5a7b97adca8c3bea9a299f3
Ítalo Silva Martins <coniik@trf.net>
2026-08-06 17:48:14 -0300

    Matéria Suruí

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx"; // 1. Nova importação do MDX adicionada aqui
 
 export default defineConfig({
   // Apenas isso deve estar aqui
   site: 'https://www.emguapi.com', // URL do site
   
-  integrations: [tailwind(), sitemap()],
+  // 2. mdx() adicionado na lista abaixo
+  integrations: [tailwind(), sitemap(), mdx()], 
 
   adapter: vercel(),
 });
